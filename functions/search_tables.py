7e8cbb98f7ec004346193aedde76776add9aab71
Carlos Aranguren <arangurencg2@gmail.com>
2024-07-15 22:50:14 -0400

    add [la constante LIST_TABLES se actualizo con nuevos nombres de tablas requeridas para que el pto de venta de a2 pueda trabajar de forma local]

--- functions/search_tables.py
+++ functions/search_tables.py
@@ -4,13 +4,14 @@ import os
 
 async def search_database_files()  -> bool :
         try:
-            LIST_TABLES = ['Sinventario', 'SSistema',  'SFixed', 'SCodebar', 'a2InvCostosPrecios', 'Soferta', 'Susuarios', 'Sclientes', 
-                       'SInvOferta', 'Smoneda', 'SUsersFlag', 'SCierresCaja', 'SclientesEventuales', 'SDetalleVenta', 'SFormasPago'] #Lista de tablas a sincronizar al iniciar app
+            LIST_TABLES = ['a2InvCostosPrecios', 'a2PtosConfig', 'Sautorizados','Sbancos', 'Sbeneficiario','Scargosfijos', 'Scategoria', 'SCierresCaja', 'Sclientes','SClientesDetalle','SclientesEventuales','SCodeBar','Scompuesto','Sconceptos','Sconvenios','Scuentasxcobrar','Sdepositos', 'SDetalleCompra', 'SDetalleInv','SDetallePartes','SDetalleVenta',
+'SFixed','SFormasPago','SFormasPagoFlag','SHistoricoCliente','SIGTF','SInstitucion','SinvDep','Sinventario','Sinvlote','SInvOferta','Smoneda','SOperacionInv','Spagosfijos',
+'Sprovinvent','SRetencioncliente','Sseriales','SSistema','Starjetas','Stransbanco','STransCuentas','SUsersFlag','Susuarios','Svendedores','Szonas','Sclasificacion'] #Lista de tablas a sincronizar al iniciar app
             file_compress = zipfile.ZipFile('data.zip', 'w')
-            result = next(os.walk('C:\\a2CA2020\\Empre001\Data'))[2] #Lista de archivos del directorio data del sistema
+            result = next(os.walk('C:\\a2CA2020\\Empre001\\Data'))[2] #Lista de archivos del directorio data del sistema
             for file in result:
                 if file[:-4] in LIST_TABLES and file.endswith(('.idx', '.dat', '.blb')):
-                    file_compress.write(filename='{path}{file_name}'.format(path='C:\\a2CA2020\\Empre001\Data\\', file_name=file), arcname=file, compress_type=zipfile.ZIP_DEFLATED)
+                    file_compress.write(filename='{path}{file_name}'.format(path='C:\\a2CA2020\\Empre001\\Data\\', file_name=file), arcname=file, compress_type=zipfile.ZIP_DEFLATED)
                 
             file_compress.close()
             return True
@@ -29,3 +30,10 @@ async def search_local_tables(path_files) -> bool:
      except Exception as e:     
           return False
      
+async def decompress_file(name_file, path_decompress):
+     try:
+          file_decompress = zipfile.ZipFile(name_file, 'r')
+          file_decompress.extractall(path=path_decompress)
+          return True
+     except Exception as e:
+          return False    
