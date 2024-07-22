078a2f7c1c3a16bedb9d3e9b3e1bcd916a87ead2
Carlos Aranguren <arangurencg2@gmail.com>
2024-07-22 16:07:24 -0400

    agregando script de powershell para mostrar aviso de desconexión al usuario sobre la ventana de windows

---
+++ modal_desconexion.ps1
@@ -0,0 +1,11 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+# Configuración de la ventana modal
+$msgBoxInput = New-Object System.Windows.Forms.MessageBoxButtons
+$msgBoxIcon = [System.Windows.Forms.MessageBoxIcon]::Exclamation
+
+# Mostrar el mensaje en una ventana modal
+$result = [System.Windows.Forms.MessageBox]::Show("Hemos detectado una desconexión de red, presione ACEPTAR, si desea entrar en modo contingencia", "Atención", $msgBoxInput::OKCancel, $msgBoxIcon)
+
+# Devolver el resultado como código de salida (OK = 1, Cancel = 2)
+exit $result
