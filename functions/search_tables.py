85074f4da1f7b401a63b0446d98b09872018248e
Carlos Aranguren <arangurencg2@gmail.com>
2024-08-05 16:32:13 -0400

    la variable catalogname se encarga de recibir la ruta donde encuentra la data o tb del sistema a2

--- functions/search_tables.py
+++ functions/search_tables.py
@@ -2,16 +2,18 @@ import zipfile
 import os
 
 
-async def search_database_files()  -> bool :
+async def search_database_files(catalogname)  -> bool :
         try:
             LIST_TABLES = ['a2InvCostosPrecios', 'a2PtosConfig', 'Sautorizados','Sbancos', 'Sbeneficiario','Scargosfijos', 'Scategoria', 'SCierresCaja', 'Sclientes','SClientesDetalle','SclientesEventuales','SCodeBar','Scompuesto','Sconceptos','Sconvenios','Scuentasxcobrar','Sdepositos', 'SDetalleCompra', 'SDetalleInv','SDetallePartes','SDetalleVenta',
 'SFixed','SFormasPago','SFormasPagoFlag','SHistoricoCliente','SIGTF','SInstitucion','SinvDep','Sinventario','Sinvlote','SInvOferta','Smoneda','SOperacionInv','Spagosfijos',
 'Sprovinvent','SRetencioncliente','Sseriales','SSistema','Starjetas','Stransbanco','STransCuentas','SUsersFlag','Susuarios','Svendedores','Szonas','Sclasificacion'] #Lista de tablas a sincronizar al iniciar app
             file_compress = zipfile.ZipFile('data.zip', 'w')
-            result = next(os.walk('C:\\a2CA2020\\Empre001\\Data'))[2] #Lista de archivos del directorio data del sistema
+            result = next(os.walk(catalogname))[2] #Lista de archivos del directorio data del sistema
+            print(result)
             for file in result:
                 if file[:-4] in LIST_TABLES and file.endswith(('.idx', '.dat', '.blb')):
-                    file_compress.write(filename='{path}{file_name}'.format(path='C:\\a2CA2020\\Empre001\\Data\\', file_name=file), arcname=file, compress_type=zipfile.ZIP_DEFLATED)
+                    print(file)
+                    file_compress.write(filename='{path}{file_name}'.format(path=f'{catalogname}\\', file_name=file), arcname=file, compress_type=zipfile.ZIP_DEFLATED)
                 
             file_compress.close()
             return True
