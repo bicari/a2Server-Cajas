690190ca39192b2ece005368ae331de73b6f79f5
Carlos Aranguren <arangurencg2@gmail.com>
2024-08-05 16:34:29 -0400

    ruta del odbc y mejora de la funcion de envio de data al servidor

--- pages/page_sync.py
+++ pages/page_sync.py
@@ -2,6 +2,14 @@ import flet as ft
 from socketio import AsyncClient
 import asyncio
 import datetime
+from querys import sqlQuerys
+from functions import getConfigClient
+import time
+config  = getConfigClient()
+PATH_DSN_ODBC =r"""DSN=A2GKC;DRIVER={path};ConnectionType=Local;RemoteIPAddress=127.0.0.1;RemotePort=12005;RemoteCompression=0;
+RemotePing=False;RemotePingInterval=60;RemoteEncryption=False;RemoteEncryptionPassword=elevatesoft;RemoteReadAhead=50;CatalogName={catalogname};ReadOnly=False;
+LockRetryCount=15;LockWaitTime=100;ForceBufferFlush=False;
+StrictChangeDetection=False;PrivateDirectory={catalogname}""".format(catalogname=config[2], path=r'C:\Program Files (x86)\DBISAM 4 ODBC-CS\libs\dbodbc\read-write\win64\dbodbc.dll')
 class SyncPage(ft.Container):
     def __init__(self, page: ft.Page, client_socket: AsyncClient, list_view_send_data: ft.ListView, progress_ring: ft.ProgressRing):
         super().__init__()
@@ -20,15 +28,24 @@ class SyncPage(ft.Container):
         self.async_client = client_socket
         self.send_message = ft.SnackBar(ft.Text(), visible=True, duration=4000, bgcolor=ft.colors.RED_300)
         self.progress_ring = progress_ring
-
+        self.modal_dialog = ft.AlertDialog(modal=True, title=ft.Text('Por favor confirme'), content=ft.Text('Desea enviar datos al server?'), actions=[ft.ElevatedButton('Sí', on_click=self.entry_emit_send_data
+                                                                                                                                                                    ), ft.OutlinedButton('No', on_click=self.modal_no_click)],
+                                                                                                                                                                    actions_alignment=ft.MainAxisAlignment.END)
+        self.column_progress_ring = ft.Column(controls=[self.progress_ring],alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.END, expand=True, width=650)
     async def emit_send_data(self):#Funcion asincrona(Corrutina) que emite el evento de envio de datos al server
         try:
-            await self.async_client.emit('update_so_sd_local', namespace='/default')
             self.container_list_view.visible = True
             self.list_view_data.controls.append(ft.Text('{hora} Ejecutando solicitud, esperando respuesta del servidor'.format(hora=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))))
-            self.page.overlay.append(ft.Column(controls=[self.progress_ring], alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.END, expand=True, width=650))
-            self.send_message.content = self.progress_ring
+            #self.page.overlay.append(ft.Column(controls=[self.progress_ring], alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.END, expand=True, width=650))
+            #self.page.overlay.append(self.column_progress_ring)
             self.page.update()
+            time.sleep(5)
+            if await sqlQuerys(PATH_DSN_ODBC).get_data_local() > 0:
+                await self.async_client.emit('update_so_sd_local', namespace='/default')
+            else:
+                self.list_view_data.controls.append(ft.Text('{hora} Tablas locales sin datos para enviar'.format(hora=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")), color= ft.colors.RED_400))   
+                self.progress_ring.visible = False
+                self.page.update()
         except Exception as e:
              self.send_message.content = ft.Text('Error de transmision, no conectado al servidor {}'.format(e))
              self.send_message.open = True
@@ -36,9 +53,23 @@ class SyncPage(ft.Container):
              self.page.update()   
              
 
-    def entry_emit_send_data(self):#Entrada a la funcion asincrona
+    def entry_emit_send_data(self, e):#Entrada a la funcion asincrona
+        self.modal_dialog.open = False
+        self.page.update()
+        print(len(self.page.overlay), self.page.overlay)
         asyncio.run(self.emit_send_data())    
+    
+    def modal_no_click(self, e):
+        self.modal_dialog.open = False
+        self.page.update()
+        
+        
+        
 
     def clic_btn_sync_data(self, e):
-        self.entry_emit_send_data()
+        self.modal_dialog.visible = True
+        self.modal_dialog.open = True
+        self.page.open(self.modal_dialog)
+        self.page.update()
+        
        
